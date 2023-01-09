ce0decc3397e42828f3a2fa1b5169bd3d30db912
dhaniarfebrinm <dhaniarfebrinwahyudi@gmail.com>
2023-01-09 07:47:06 +0700

    Add project files.

---
+++ Models/Rooms.cs
@@ -0,0 +1,11 @@
+﻿namespace resorty.Models
+{
+    public class Rooms
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Floor { get; set; }
+        public string Status { get; set; }
+        public int? Price { get; set; }
+    }
+}
