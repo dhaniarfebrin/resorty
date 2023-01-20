5ff784b3d3995abd5b640facd8449ace576161d8
dhaniarfebrinm <dhaniarfebrinwahyudi@gmail.com>
2023-01-20 16:01:52 +0700

    finalization

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -241,19 +241,18 @@ namespace resorty.Controllers
         // GET: Reservations/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.Reservation == null)
+            if (_context.Reservation == null)
             {
-                return NotFound();
+                return Problem("Entity set 'resortyContext.Reservation'  is null.");
+            }
+            var reservation = await _context.Reservation.FindAsync(id);
+            if (reservation != null)
+            {
+                _context.Reservation.Remove(reservation);
             }
 
-            var reservation = await _context.Reservation
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-
-            return View(reservation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(HistoryReservations));
         }
 
         // POST: Reservations/Delete/5
